61cca3f59fc74d309bdef874727919596a61ae3c
Karen <karapetyankaren33933@gmail.com>
2024-10-20 21:28:20 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -32,54 +32,56 @@ public class Main {
         int year = 35;
 
         if (year >= 2 && year <= 6) {
-            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в детский сад");}
+            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в детский сад");
+        }
 
-        if (year >= 7 && year <=17) {
-            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в школу");}
+        if (year >= 7 && year <= 17) {
+            System.out.println("Если возраст человека равен " + year + ", то ему нужно ходить в школу");
+        }
 
-        if (year >= 18  && year <= 24){
-            System.out.println("Если возраст человека равен " + year + ", то его место в университете");}
+        if (year >= 18 && year <= 24) {
+            System.out.println("Если возраст человека равен " + year + ", то его место в университете");
+        }
 
         if (year > 24) {
             System.out.println("Если возраст человека равен " + year + ", то ему пора ходить на работу");
         }
 
 
-
         int childYear = 13;
-        if (childYear < 5){
+        if (childYear < 5) {
             System.out.println("Если возраст ребенка равен " + childYear + ", то ему нельзя кататься на аттракционе");
         }
-        if (childYear >= 5  && childYear < 14){
+        if (childYear >= 5 && childYear < 14) {
             System.out.println("Если возраст ребенка " + childYear + ", то ему можно кататься на аттракционе в сопровождении взрослого");
         }
-        if (childYear >= 14){
+        if (childYear >= 14) {
             System.out.println("Если возраст ребенка " + childYear + ", то ему можно кататься на аттракцыоне без сопровождения взрослого");
         }
 
 
-
         int capacityVan = 102;
-        if (capacityVan >= 60){
+        if (capacityVan >= 60) {
             System.out.println("Есть место в вагоне - стоячие");
         } else {
             System.out.println("Вагон уже полностью забит");
         }
 
 
-
         int one = 1;
         int two = 2;
         int three = 3;
-        if (one > two && one > three){
+        if (one > two && one > three) {
             System.out.println(one);
 
-        }else {
+        } else {
             if (two > three) {
                 System.out.println(two);
 
-            }else {
+            } else {
                 System.out.println(three);
-    }
+            }
 
+        }
+    }
 }
